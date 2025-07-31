a00567b22f927256300221cf07e8e4e13c145499
Marsó Mónika <marsom@vector.local>
2025-07-31 15:59:51 +0200

    tab filecange

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 $requestUri = $_SERVER['REQUEST_URI'];
 
 if (str_starts_with($requestUri, '/api/ugyfelData')) {
@@ -13,14 +12,12 @@ if (str_starts_with($requestUri, '/api/cikkData')) {
     exit;
 }
 
-// Ha nem API, akkor jöhet a normál oldal betöltés
-define('ROOT', substr(($_SERVER['DOCUMENT_ROOT'] ?? ""), 0, 0));
-
 require_once __DIR__ . '/../src/modules/CikkList.php';
 require_once __DIR__ . '/../src/modules/UgyfelList.php';
 
 use App\modules\CikkList;
 use App\modules\UgyfelList;
+
 ?>
 
 <!DOCTYPE html>
@@ -29,33 +26,43 @@ use App\modules\UgyfelList;
     <meta charset="UTF-8">
     <title>DataTables MSSQL</title>
 
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css">
 </head>
 <body>
-<button id="btnCikkekTable">Cikkek</button>
-<button id="btnUgyfelekTable">Ügyfelek</button>
-<?php
-print UgyfelList::getUgyfelTemplate();
-print CikkList::getCikkTemplate();
-?>
 
-<link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css">
-<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#cikkTableBox">Cikk</a></li>
+    <li><a data-toggle="tab" href="#ugyfelTableBox">Ügyfél</a></li>
+</ul>
+
+<div class="tab-content">
+    <div id="cikkTableBox" class="tab-pane fade in active">
+        <?php print CikkList::getCikkTemplate(); ?>
+    </div>
+    <div id="ugyfelTableBox" class="tab-pane fade">
+        <?php print UgyfelList::getUgyfelTemplate(); ?>
+    </div>
+</div>
+
+<!-- Scripts: jQuery → Bootstrap → DataTables -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/2.3.2/js/dataTables.js"></script>
-<script>
 
+<script>
     $(document).ready(function () {
-        $('#btnCikkekTable').click(function () {
-            $('#cikkTableBox').show();
-            $('#ugyfelTableBox').hide()
-        })
-        $('#btnUgyfelekTable').click(function () {
-            $('#ugyfelTableBox').show();
-            $('#cikkTableBox').hide();
-        })
-        <?php
-        print CikkList::getJs();
-        print UgyfelList::getjs();
-        ?>
+        // A Bootstrap data-toggle automatikusan kezeli a tab váltást
+        // Nem szükséges: $(...).tab('show')
     });
+
+    <?php
+    print CikkList::getJs();
+    print UgyfelList::getjs();
+    ?>
 </script>
+
+</body>
 </html>
